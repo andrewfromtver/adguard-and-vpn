9c6385b2943632ef350bbbfe262ca5e5af41105c
Andranik Sarkisyan <andrewsarkisyan@gmail.com>
2023-04-06 23:51:20 +0300

    -

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,7 @@ NGINX_USERNAME=$(cat ./.creds | grep NGINX_USERNAME | cut -d "=" -f 2)
 NGINX_PASSWORD_HASH=$(cat ./.creds | grep NGINX_PASSWORD_HASH | cut -d "=" -f 2)
 ADGUARD_DNS_IP=$(cat ./.env | grep ADGUARD_DNS_IP | cut -d "=" -f 2)
 OPENVPN_SERVER_SUBNET=$(cat ./.env | grep OPENVPN_SERVER_SUBNET | cut -d "=" -f 2)
+GLOBAL_VPN_ADDRESS=$(cat ./.env | grep GLOBAL_VPN_ADDRESS | cut -d "=" -f 2)
 
 mkdir -p ./.app_data/adguard/conf/
 mkdir -p ./.app_data/custompasswd/
@@ -29,15 +30,18 @@ mkdir -p ./.app_data/openvpn/conf/
 cp ./adguard/AdGuardHome.yaml ./.app_data/adguard/conf/AdGuardHome.yaml
 cp ./nginx/.htpasswd ./.app_data/custompasswd/.htpasswd
 cp ./openvpn/config/* ./.app_data/openvpn/conf/
+cp ./monitoring/* ./.app_data/monitoring/
 
-sed -i "s/ADGUARD_USERNAME/$ADGUARD_USERNAME/" ./.app_data/adguard/conf/AdGuardHome.yaml
-sed -i "s/ADGUARD_PASSWORD_HASH/$ADGUARD_PASSWORD_HASH/" ./.app_data/adguard/conf/AdGuardHome.yaml
+sed -i "s/ADGUARD_USERNAME/$ADGUARD_USERNAME/g" ./.app_data/adguard/conf/AdGuardHome.yaml
+sed -i "s/ADGUARD_PASSWORD_HASH/$ADGUARD_PASSWORD_HASH/g" ./.app_data/adguard/conf/AdGuardHome.yaml
 
-sed -i "s/NGINX_USERNAME/$NGINX_USERNAME/" ./.app_data/custompasswd/.htpasswd
-sed -i "s/NGINX_PASSWORD_HASH/$NGINX_PASSWORD_HASH/" ./.app_data/custompasswd/.htpasswd
+sed -i "s/NGINX_USERNAME/$NGINX_USERNAME/g" ./.app_data/custompasswd/.htpasswd
+sed -i "s/NGINX_PASSWORD_HASH/$NGINX_PASSWORD_HASH/g" ./.app_data/custompasswd/.htpasswd
 
-sed -i "s/ADGUARD_DNS_IP/$ADGUARD_DNS_IP/" ./.app_data/openvpn/conf/server.conf
-sed -i "s/OPENVPN_SERVER_SUBNET/$OPENVPN_SERVER_SUBNET/" ./.app_data/openvpn/conf/server.conf
+sed -i "s/ADGUARD_DNS_IP/$ADGUARD_DNS_IP/g" ./.app_data/openvpn/conf/server.conf
+sed -i "s/OPENVPN_SERVER_SUBNET/$OPENVPN_SERVER_SUBNET/g" ./.app_data/openvpn/conf/server.conf
+
+sed -i "s/GLOBAL_VPN_ADDRESS/$GLOBAL_VPN_ADDRESS/g" ./.app_data/monitoring/prometheus.yml
 
 docker compose -p private-vpn down --remove-orphans
 docker compose -p private-vpn build
